59f730ffc1c87ba7e6dcbbe27609037ca9496424
Gianni Tedesco <gianni@scaramanga.co.uk>
2011-01-02 04:34:24 +0000

    Link to libraries wrapped by python extension modules

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup, Extension
 incs = ['include']
 mods = ['ccid', 'emv']
-exts = [Extension(x, ['src/py_%s.c'%x], include_dirs=incs) for x in mods]
+exts = [Extension(x, ['src/py_%s.c'%x],
+			library_dirs=['./src/.libs/'],
+			libraries=[x],
+			include_dirs=incs)
+	for x in mods]
 setup(name='ccid-utils', version='0.2', ext_modules=exts)
